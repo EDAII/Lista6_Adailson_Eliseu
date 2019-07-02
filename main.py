5eacde075fd7153aacd2037710c6344ed2f9ef04
Adailson <adailson2@gmail.com>
2019-07-01 22:37:45 -0300

    Adicionando gráfico comparativo

--- main.py
+++ main.py
@@ -1,41 +1,101 @@
-import random, time
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import os, random, time
 from graph import Graph
 
 number = 1
-g = Graph()
+values = []
+n=0
+try:
+    g = Graph()
+except:
+    print('Exception occurred')
 
-def generate_graph():
+def comparation_graph(result_search,n):
+    plt.ylabel('Tempo para %d nós (em milisegundos)' % (n))
+    
+    for key in result_search.keys():
+        plt.bar(key, result_search[key])
+
+    plt.show()
+
+def search_results(n, key):
     g.addEdge(0,1)
     i = 1
-    for i in range(1,15):
-        k = random.randrange(1,15)
-        print("g.addEdge(" + str(k) + "," + str(i)+")")
-        print("g.addEdge(" + str(i) + "," + str(k)+")")
+    for i in range(1,n):
+        k = random.randrange(1,n)
+        # print("g.addEdge(" + str(k) + "," + str(i)+")")
+        # print("g.addEdge(" + str(i) + "," + str(k)+")")
         g.addEdge(k,i)
         g.addEdge(i,k)
-    start = time.time()
-    print(g.BFS(0))
-    end = time.time()
-    diff = float(end - start)
-    diff = diff * 1000
-    print(diff)
-    start = time.time()
-    print(g.DFS(0))
-    end = time.time()
-    diff = float(end - start)
-    diff = diff * 1000
-    print(diff)
-    
+    if(key == "BFS"):
+        return g.BFS(0)
+    else:
+        return g.DFS(0)
+
+results_search = {'BFS': [], 'DFS': []}
+result_search = {'BFS': 0, 'DFS': 0}
+
 while(number):
+    start = 0
+    end = 0
+    value = 0
+    values.clear
+    value_sought = 0
+    diff = 0
+    i = 0
     print("Escolha uma das opções:\n"
-        +"1- Gerar grafo randômico\n"
-        +"2- Criar novo grafo\n")
+        +"1- Comparativo de busca BFS x DFS (busca valor randômico em valores para n nós)\n"
+        +"0- Para sair\n")
     number = int(input())
-    if number == 1:
-        generate_graph()
-        print("Escolha uma das opções:\n"
-        +"1- Percorrer grafo (BFS)\n"
-        +"2- Percorrer grafo (DFS)\n"
-        +"3- Analizar ciclo ímpar\n"
-        +"4- Definir menor caminho entre s e t")
-        choice = int(input())
+    if number == 1 or number == 2 or number == 3:
+        print("Digite a quantidade de nós:")
+        n = int(input())
+        if number == 1:
+            print("Digite quantas iterações você deseja buscar para %d nós:" % n)
+            b = int(input())
+        for key in result_search.keys():
+            if key == 'DFS': 
+                g = Graph()
+                result_dfs = None
+                if number == 1:
+                    start = time.time()
+                    for i in range(0,b):
+                        result_dfs = search_results(n,key)
+                elif number == 2:
+                    start = time.time()
+                    for i in range(0,b):
+                        result_dfs = search_results(n,key)
+                else:
+                    start = time.time()
+                    for i in range(0,b):
+                        result_dfs = search_results(n,key)
+
+            elif key == 'BFS':
+                if number == 1:
+                    start = time.time()
+                    for i in range(0,b):
+                        result_bfs = search_results(n,key)
+                elif number == 2:
+                    start = time.time()
+                    for i in range(0,b):
+                        result_bfs = search_results(n,key)
+                else:
+                    start = time.time()
+                    for i in range(0,b):
+                        result_bfs = search_results(n,key)
+
+            end = time.time()
+            diff = float(end - start)
+            diff = diff * 1000
+            
+            if number == 1:
+                result_search[key] = diff
+                results_search[key].append(diff)
+
+        if number == 1:
+            comparation_graph(result_search, n)
+
+    print("Aperte enter para continuar!")
+    input()
+    os.system('clear')
